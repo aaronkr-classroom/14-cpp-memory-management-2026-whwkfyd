466e4a5289f75acbcd5a8cadf353c303da28f487
ththfql <145094239+ththfql@users.noreply.github.com>
2026-06-10 16:21:10 +0900

    수정

--- Chp 13/Ptr.h
+++ Chp 13/Ptr.h
@@ -1,67 +1,72 @@
 #pragma once
-
+// Ptr.h
 #ifndef GUARD_Ptr_h
 #define GUARD_Ptr_h
 
 #include <cstddef>
 #include <stdexcept>
-#include "Vec.h"
+#include "Vec.h" // 추가
 
 using namespace std;
 
 template <class T> class Ptr {
 public:
+	// 필요하면 조건에 따라 객체를 복사하는 새로운 멤버 함수
 	void make_unique() {
 		if (*refptr != 1) {
 			--*refptr;
-			refptr = new size_t(1);
-			p = p ? clone(p) : 0;
-	}
+			refptr = new size_t(1); // spelling 확인
+			// p = p ? p->clone() : 0;
+			p = p ? clone(p) : 0; // clone(p)로
+		}
 	}
 
+	// 참조 카운트를 포인터처럼 관리
 	Ptr() : p(0), refptr(new size_t(1)) { }
 	Ptr(T* t) : p(t), refptr(new size_t(1)) { }
 	Ptr(const Ptr& h) : p(h.p), refptr(h.refptr) {
 		++*refptr;
 	}
-	//할당연산자
+	// 할당 연산자
 	Ptr& operator=(const Ptr&);
-	~Ptr();
+	~Ptr(); // 소멸자
 
+	// 이전과 같음 (Handle 클래스)
 	operator bool() const { return p; }
 	T& operator*() const {
 		if (p) return *p;
 		throw runtime_error("unbound Ptr!");
 	}
 	T* operator->() const {
-		if (p) return *p;
-		throw runtime_error("Ptr!");
+		if (p) return p;
+		throw runtime_error("unbound Ptr!");
 	}
 private:
 	T* p;
 	size_t* refptr;
 };
 
-template <class T> T* clone(const T* tp);
+// clone() 함수의 2개의 경우 (수서 문제 -> 원래 원형 있었죠)
+template <class T> T* clone(const T* tp) {
+	return tp->clone();
+}
 
 template <>
 Vec<char>* clone(const Vec<char>* vp) {
 	return new Vec<char>(*vp);
 }
 
-template <class T> T* clone(const T* tp) {
-	return tp->clone();
-}
-
 template <class T>
-Ptr<T>& Ptr<T>::operator bool(const Ptr& rhs) {
-	++*rhs.refptr;
+Ptr<T>& Ptr<T>::operator=(const Ptr& rhs) {
+	++*rhs.refptr; // 참조 포인터 이동
 
-	if (--*refptr == 0) {
+	// 상황에 따라 좌별을 할당 해제하고 포인터 소멸
+	if (--*refptr == 0) { // 확인 / 비교 NOT 할당
 		delete refptr;
 		delete p;
 	}
 
+	// 우변 값들을 복사
 	refptr = rhs.refptr;
 	p = rhs.p;
 
@@ -70,7 +75,7 @@ Ptr<T>& Ptr<T>::operator bool(const Ptr& rhs) {
 
 template <class T>
 Ptr<T>::~Ptr() {
-	if (--*refptr == 0) {
+	if (--*refptr == 0) { // 확인 / 비교 NOT 할당
 		delete refptr;
 		delete p;
 	}
--- Chp 13/Str.h
+++ Chp 13/Str.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+// Str.h
 #ifndef GUARD_STR_H
 #define GUARD_STR_H
 
@@ -6,49 +7,76 @@
 #include <iterator>
 #include <cstring>
 #include "Vec.h"
+#include "Ptr.h" // NEW
 
 using namespace std;
+
 class Str {
+	// friend 사용하면 private data 접근할 수 있다
+	friend istream& operator>>(istream&, Str&); // 입력할 때 const 없어요
+	friend istream& getline(istream&, Str&); // NEW
+
 public:
 	typedef Vec<char>::size_type size_type;
 	typedef char* iterator;
 	typedef const char* const_iterator;
 
-	//생성자들
-	Str() : data(new Vec<char>) { }
+	// 생성자들
+	// 1. 비어 있는 Str 객체를 만드는 기본 생성자
+	Str() : data(new Vec<char>) { } // 초기값 없다
 
+	// 2. c의 복사본 n개가 있는 Str 객체를 생성
 	Str(size_type n, char c) : data(new Vec<char>(n, c)) { }
 
-	Str(const char* cp) : data(new Vec<char) {
-		copy(
-			cp, cp + strlen(cp),
-			back_inserter(data)
+	// 3. null로 끝나는 char 타입의 배열에서 Str 객체를 생성
+	Str(const char* cp) : data(new Vec<char>) {
+		copy( // std::copy()
+			cp, cp + strlen(cp), // std::strlen()
+			back_inserter(*data)  // std::back_inserter() -> 역방향 삽입
 		);
 	}
 
-	template<class In> Str(In b, In e) : data(new Vec<char>)
-		copy(b, e, back_inserter(data));
+	// 4. 반복자 b와 e가 가리키는 범위에서 Str 객체를 생성
+	template<class In> Str(In b, In e) : data(new Vec<char>) {
+		copy(b, e, back_inserter(*data));
 	}
 
+	// 길이/크기 함수
 	size_type size() const { return data->size(); }
+
+	// 연산자 : 인덱스
 	char& operator[](size_type i) {
 		data.make_unique();
-		return (*data)[i]; }
-const& operator
-Str& operator+=(const Str& s) {
-	data.make_unique();
-	copy(s.data.bgin(), s.data end()),
-		(back_inserter(*data));
-	return *this;
-}
+		return (*data)[i];
+	}
+	const char& operator[](size_type i) const { return (*data)[i]; }
 
-iterator begin() { return data->begin(); }
-iterator end() { return data->end(); }
-const_iterator begin() { return data->begin(); }
-const_iterator end() { return data->end(); }
+	// 연산자 : 결합
+	Str& operator+=(const Str& s) {
+		data.make_unique();
+		copy(s.data->begin(), s.data->end(),
+			back_inserter(*data));
+		return *this;
+	}
+
+	// 연습문제 12: 반복자들
+	iterator begin() { return data->begin(); }
+	iterator end() { return data->end(); }
+
+	const_iterator begin() const { return data->begin(); }
+	const_iterator end() const { return data->end(); }
+
+	/*
+	iterator insert(iterator d, iterator b, iterator e) {
+		return data->insert(d, b, e);
+	}
+	*/
+
 private:
-	Ptr<> data;
+	Ptr< Vec<char> > data; // 수정
 };
 
-#endif // !GUARD_STR_J
+ostream& operator<<(ostream&, const Str&); // 출력 연산자
+Str operator+(const Str&, const Str&); // str1 + str2 결합 연산자
 
+#endif
